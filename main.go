22725cd314e6d5b29d2cac5f9535e5e2069d1d10
N30x <vkoval97@email.cz>
2023-05-13 18:29:18 +0200

    ADD implement group prefix to the handler

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/N30xCz/HotelReservationApi/api"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
+	appv1 := app.Group("/api/v1")
 	app.Get("/user", userHandler)
+	appv1.Get("/users", api.GetUser)
+
 	app.Listen(":5000")
 }
 func userHandler(c *fiber.Ctx) error {
