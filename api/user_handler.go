c7d16a455a14647a9572ac8da1f34c201dbec3fc
N30xCz <vkoval97@email.cz>
2023-05-14 19:15:29 +0200

    ADD user_handler
    wrapper for UserRelated handler

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,14 +1,41 @@
 package api
 
 import (
+	"context"
+
+	"github.com/N30xCz/HotelReservationApi/db"
 	"github.com/N30xCz/HotelReservationApi/types"
 	"github.com/gofiber/fiber/v2"
 )
 
-func GetUser(c *fiber.Ctx) error {
+type UserHandler struct {
+	userStore db.UserStore
+}
+
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+}
+
+func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
+	var (
+		id  = c.Params("id")
+		ctx = context.Background()
+	)
+	user, err := h.userStore.GetUserByID(ctx, id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(user)
+
+}
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
+
 	u := types.User{
 		FirstName: "Anthony",
 		LastName:  "GG",
 	}
 	return c.JSON(u)
+
 }
