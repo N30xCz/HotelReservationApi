39ebd45517c4d6559248a80754735586d4dfb60f
N30xCz <vkoval97@email.cz>
2023-05-14 19:17:40 +0200

    ADD Implement changes in main

--- main.go
+++ main.go
@@ -2,50 +2,28 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"github.com/N30xCz/HotelReservationApi/api"
-	"github.com/N30xCz/HotelReservationApi/types"
+	"github.com/N30xCz/HotelReservationApi/db"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func userHandler(c *fiber.Ctx) error {
-	return c.JSON(map[string]string{"welcome msg": "Hello From server :)"})
-
-}
-
-const dbUri = "mongodb://localhost:27017"
-const dbName = "Hotel-Reservation"
-const userColl = "users"
-
 func main() {
 	// Create MongoDB connection
+	const dburi = "mongodb://localhost:27017"
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbUri))
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Insert User to the MongoDB
-	ctx := context.Background()
-	coll := client.Database(dbName).Collection(userColl)
-	user := types.User{
-		FirstName: "Pepek",
-		LastName:  "Namornik",
-	}
-	res, err := coll.InsertOne(ctx, user)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(res)
-	// Start Fiber framework
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
 	app := fiber.New()
-	appv1 := app.Group("/api/v1")
-	app.Get("/user", userHandler)
-	appv1.Get("/users", api.GetUser)
+	apiv1 := app.Group("/api/v1")
+	apiv1.Get("/users", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
 
 	app.Listen(":5000")
-
 }
