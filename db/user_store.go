c849a81391ebab16f8c8ba63c40859250e42a258
N30xCz <vkoval97@email.cz>
2023-05-15 20:10:09 +0200

    ADD conversition to primitive to the user_store

--- db/user_store.go
+++ db/user_store.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/N30xCz/HotelReservationApi/types"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 const userColl = "users"
+const DBNAME = "Hotel-Reservation"
 
 type UserStore interface {
 	GetUserByID(context.Context, string) (*types.User, error)
@@ -25,8 +27,12 @@ func NewMongoUserStore(client *mongo.Client) *MongoUserStore {
 	}
 }
 func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.User, error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
 	var user types.User
-	if err := s.coll.FindOne(ctx, bson.M{"_id": ToObjectID(id)}).Decode(&user); err != nil {
+	if err := s.coll.FindOne(ctx, bson.M{"_id": oid}).Decode(&user); err != nil {
 		return nil, err
 	}
 	return &user, nil
