2ec0fc34855971a3a979f0102a703df112186337
N30xCz <vkoval97@email.cz>
2023-05-15 20:10:17 +0200

    ADD Error handling

--- main.go
+++ main.go
@@ -11,16 +11,23 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dburi = "mongodb://localhost:27017"
+
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
+
 func main() {
 	// Create MongoDB connection
-	const dburi = "mongodb://localhost:27017"
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
-	app := fiber.New()
+	app := fiber.New(config)
 	apiv1 := app.Group("/api/v1")
 	apiv1.Get("/users", userHandler.HandleGetUsers)
 	apiv1.Get("/user/:id", userHandler.HandleGetUser)
