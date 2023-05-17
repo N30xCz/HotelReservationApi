9e29200450613dd668d6cb2d9dfbd9b77603af5c
N30x <vkoval97@email.cz>
2023-05-17 18:40:15 +0200

    ADD GetUsers method

--- db/user_store.go
+++ db/user_store.go
@@ -14,6 +14,7 @@ const DBNAME = "Hotel-Reservation"
 
 type UserStore interface {
 	GetUserByID(context.Context, string) (*types.User, error)
+	GetUsers(context.Context) ([]*types.User, error)
 }
 type MongoUserStore struct {
 	client *mongo.Client
@@ -25,6 +26,18 @@ func NewMongoUserStore(client *mongo.Client) *MongoUserStore {
 		client: client,
 		coll:   client.Database(DBNAME).Collection(userColl),
 	}
+}
+func (s *MongoUserStore) GetUsers(ctx context.Context) ([]*types.User, error) {
+	cur, err := s.coll.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+
+	var users []*types.User
+	if err := cur.Decode(&users); err != nil {
+		return nil, err
+	}
+	return users, nil
 }
 func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.User, error) {
 	oid, err := primitive.ObjectIDFromHex(id)
