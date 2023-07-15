34cb9902b256af8c0c26a46562ea490a9f1f26b5
N30x <vkoval97@email.cz>
2023-07-15 18:13:22 +0200

    ADD Finish JWT Authentication

--- api/middleware/jwt.go
+++ api/middleware/jwt.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
@@ -15,13 +16,20 @@ func JWTAuthentication(c *fiber.Ctx) error {
 	if !ok {
 		return fmt.Errorf("Unautheticated")
 	}
-	if err := parseToken(token); err != nil {
-		return err
+	claims, err := ValidateToken(token)
+	if err != nil {
+		fmt.Errorf("unauthorized")
 	}
-	return nil
+	expiresFloat := claims["expires"].(float64)
+	expires := int64(expiresFloat)
+	// check token expiration
+	if time.Now().Unix() > expires {
+		return fmt.Errorf("token is expired")
+	}
+	return c.Next()
 }
 
-func parseToken(tokenStr string) error {
+func ValidateToken(tokenStr string) (jwt.MapClaims, error) {
 	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
 		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
@@ -31,16 +39,21 @@ func parseToken(tokenStr string) error {
 
 		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
 		secret := os.Getenv("JWT_SECRET")
-		fmt.Println("DELETE ME AFTER DEBUG SECRET IS ", secret)
 		return []byte(secret), nil
 	})
 	if err != nil {
 		fmt.Println("Failed to parse JWT Token", err)
-		return fmt.Errorf("Unauthorized")
+		return nil, fmt.Errorf("Unauthorized")
 	}
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		fmt.Println(claims)
+	if !token.Valid {
+		fmt.Println("Invalid Token")
+		return nil, fmt.Errorf("unauthorized")
 	}
-	return fmt.Errorf("unauthorized")
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, fmt.Errorf("unauthorized")
+	}
+
+	return claims, nil
 
 }
